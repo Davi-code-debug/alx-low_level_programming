caceb03829c8005dac3d6072548214666cb648ce
Davi-code-debug <oluwaseyioke20@gmail.com>
2022-07-03 19:35:54 -0700

    alphabets excluding q & e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * alp: contains alphabet
+ *
+ * prints out the alphabets excluding q & e
+ * Return: 0 after success
+ */
+int main(void)
+{
+	char alp = 'a';
+
+	while (alp <= 'z' || (alp != 'q' && alp != 'e'))
+	{
+	putchar(alp);
+	alp++;
+	}
+	putchar('\n');
+
+	return (0);
+}
