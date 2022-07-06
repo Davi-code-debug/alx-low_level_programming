ad0ea95169a83f406e549a02c7293ca52dcc94a4
Davi-code-debug <oluwaseyioke20@gmail.com>
2022-07-05 18:32:25 -0700

    lowercases x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+
+/**
+ *print_alphabet_x10 - prints lowercases a to z 10 times.
+ *
+ *Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char ch = 'a';
+
+	while (no <= 10)
+	{
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+	_putchar('\n');
+	}
+}
