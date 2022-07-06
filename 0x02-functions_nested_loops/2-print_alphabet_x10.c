275617bc2bb1db53f25e78d29ac1ef6057791d8c
Davi-code-debug <oluwaseyioke20@gmail.com>
2022-07-05 19:02:38 -0700

    lowercases x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,14 +12,14 @@ void print_alphabet_x10(void)
 	int no = 0;
 	char ch = 'a';
 
-	while (no <= 10)
+	while (no <= 9)
 	{
 		while (ch <= 'z')
 		{
 			_putchar(ch);
 			ch++;
 		}
+	n++;
 	_putchar('\n');
-	no++;
 	}
 }
