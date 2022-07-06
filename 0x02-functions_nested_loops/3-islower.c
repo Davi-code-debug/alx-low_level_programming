05cf564003d8c50592648c9974f9883c6023b142
Davi-code-debug <oluwaseyioke20@gmail.com>
2022-07-05 20:13:11 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower - displays the value of lower and uppercases
+ *c: contains the letter
+ *
+ * display the return value of c
+ * Return: 0 after success
+ */
+int _islower(int c)
+{
+	c = islower('H');
+	_putchar(c + '0');
+	c = islower('o');
+	_putchar(c + '0');
+	c = islower(108);
+	_putchar(c + '0');
+	_putchar('\n');
+
+	return (0);
+}
