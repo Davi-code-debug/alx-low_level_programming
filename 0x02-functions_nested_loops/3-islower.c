ccbe17470d5293da2a0e72cf34707be455c5be17
Davi-code-debug <oluwaseyioke20@gmail.com>
2022-07-05 20:15:46 -0700

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,11 +8,11 @@
  */
 int _islower(int c)
 {
-	c = islower('H');
+	c = _islower('H');
 	_putchar(c + '0');
-	c = islower('o');
+	c = _islower('o');
 	_putchar(c + '0');
-	c = islower(108);
+	c = _islower(108);
 	_putchar(c + '0');
 	_putchar('\n');
 
