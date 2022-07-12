0cec8ca6b9d3db266df25e3b728abbc44466f19a
Davi-code-debug <oluwaseyioke20@gmail.com>
2022-07-12 15:52:36 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - calc length of string
+ * @s: contains string
+ *
+ * calcs length of string
+ * Return: 0 after success
+ */
+int _strlen(char *s)
+{
+	strlen(s);
+}
