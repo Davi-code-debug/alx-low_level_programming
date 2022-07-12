bcb143a338e49f312239ea03bdfa5ba2098c7e6c
Davi-code-debug <oluwaseyioke20@gmail.com>
2022-07-12 13:27:35 -0700

    task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * swap_int - function that swaps the value of integers
+ * @a: first int
+ * @b: second int
+ *
+ * swaps values of two int var
+ * Return: 0 after success
+ */
+void swap_int(int *a, int *b)
+{
+	*a = &b;
+	*b = &a;
+}
