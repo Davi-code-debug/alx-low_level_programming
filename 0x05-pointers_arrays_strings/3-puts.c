58c072c25fb327288c13c7ae13600ce686bc2dff
Davi-code-debug <oluwaseyioke20@gmail.com>
2022-07-12 19:23:01 -0700

    task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * _puts - performs the function of put
+ * @n: contains the string
+ *
+ * prints out a string
+ * Return:void
+ */
+void _puts(char *str)
+{
+	char *p = str;
+
+	while (*p != '\0')
+	{
+		_putchar(p);
+	}
+}
