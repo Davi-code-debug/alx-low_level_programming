df8d3c1833c4582750f059e47bb1f95fca30a75a
Davi-code-debug <oluwaseyioke20@gmail.com>
2022-07-19 11:48:22 -0700

    task2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,8 +4,6 @@
  * _strchr - funtion that checks for a specified character
  * @s: pointer to string
  * @c: contains character to check for
- * i: increments
- * v: new array
  *
  * checks for specified character
  * Return: pointer to occurence of letter
@@ -13,14 +11,11 @@
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int i;
-	char v[i];
-
-	for (i = 0; *s != '\0'; i++)
+	while (*s != '\0')
 	{
-		*(v + i) = *(s + i);
-		if (*(v + i) == c)
+		if (*s == c)
 		return (s);
+		s++;
 	}
 		return (NULL);
 }
