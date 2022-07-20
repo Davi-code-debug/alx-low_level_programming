8d35367543f30184e800f025b1305f06fa992066
Davi-code-debug <oluwaseyioke20@gmail.com>
2022-07-19 20:26:42 -0700

    task6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_chessboard - prints an 8 by 8 matrix
+ * @a: points to a
+ * i: row
+ * j: column
+ *
+ * prints a 2 dimensional array
+ * Return: void after success
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+	
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
