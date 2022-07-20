0338a316651502ea6449116ca424cfaa0ec6f3e0
Davi-code-debug <oluwaseyioke20@gmail.com>
2022-07-19 19:23:54 -0700

    task5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strstr - finds a substring in a string
+ * @haystack: points to string
+ * @needle: points to substring
+ *
+ *locates a substring
+ * Return: haystack at the first occurrence of
+ * needle
+ * returns null if there is no occurence
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		if (*haystack == *needle)
+			return (needle);
+		haystack++;
+	}
+	return (NULL);
+}
