2448acbedfb8b58e2f0fb5e3dfe8b6115494cecf
Davi-code-debug <oluwaseyioke20@gmail.com>
2022-07-21 15:45:40 -0700

    task4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _pow_recursion - prints integer to a specified power
+ * @x: stores integer
+ * @y: stores power
+ *
+ * prints the value of an integer to a power
+ * return -1 if y is less than 0
+ * rEturn 1 if y is 0
+ * Return: value of integer
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	
+	if (y == 0)
+		return (1);
+	return (x *_pow_recursion(x, y - 1));
+}
