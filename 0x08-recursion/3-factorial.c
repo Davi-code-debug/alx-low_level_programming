2c5a641248566cd42d7d474a46373df95674d17e
Davi-code-debug <oluwaseyioke20@gmail.com>
2022-07-21 15:03:58 -0700

    task3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 /**
+ * factorial - prints factorial of a number
+ * @n: stores integer
+ * i: variable constant
+ *
+ * prints factorial
+ * Return: i if n is 0
+ * RETURN: -1 if n is less than 0
+ * return the factorial of the integer
  */
 int factorial(int n)
 {
