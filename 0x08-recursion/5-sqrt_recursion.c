8ba4396f1e4be51ef2484ce0b27a1fd4ede15fb6
Davi-code-debug <oluwaseyioke20@gmail.com>
2022-07-21 18:36:41 -0700

    task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,10 +9,11 @@ int _sqrt(int n, int i);
  *
  * calls a function
  * Return: -1 if n is less than 0
+ * return recursion
  */
 int _sqrt_recursion(int n)
 {
-	if ( n < 0 )
+	if (n < 0)
 		return (-1);
 	return _sqrt(n, 0);
 }
@@ -25,6 +26,7 @@ int _sqrt_recursion(int n)
  * finds square root
  * returns -1 if i * i > n
  * Return: i after success
+ * returN recursion
  */
 int _sqrt(int n, int i)
 {
@@ -34,5 +36,5 @@ int _sqrt(int n, int i)
 	}
 	if (i * i == n)
 		return (i);
-	return _sqrt(n, (i + 1));
+	return (_sqrt(n, (i + 1)));
 }
