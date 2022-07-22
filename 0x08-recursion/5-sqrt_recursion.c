9b1c46e1164ac444e140fa1f5b78e3278bee3caa
Davi-code-debug <oluwaseyioke20@gmail.com>
2022-07-21 18:29:52 -0700

    task5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+int _sqrt(int n, int i);
+
+/**
+ * _sqrt_recursion - calls the function _sqrt
+ * @n: stores integer
+ *
+ * calls a function
+ * Return: -1 if n is less than 0
+ */
+int _sqrt_recursion(int n)
+{
+	if ( n < 0 )
+		return (-1);
+	return _sqrt(n, 0);
+}
+
+/**
+ * _sqrt - gets the square root of n
+ * @n: stores the ineger
+ * @i: increments andis the root of n
+ *
+ * finds square root
+ * returns -1 if i * i > n
+ * Return: i after success
+ */
+int _sqrt(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	if (i * i == n)
+		return (i);
+	return _sqrt(n, (i + 1));
+}
