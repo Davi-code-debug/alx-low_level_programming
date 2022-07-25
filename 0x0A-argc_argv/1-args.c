2abe83689bb82f1c2b79e024a2c5da43259afc23
Davi-code-debug <oluwaseyioke20@gmail.com>
2022-07-25 03:17:04 -0700

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of strings in a command line
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * prints number of strings
+ * Return: 0 after success
+ */
+int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
+{
+	{
+	printf("%d\n", argc - 1);
+	}
+	return (0);
+}
