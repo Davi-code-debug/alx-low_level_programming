ebd81d93e0bfc7ff1f57bb0641f8d2741ad93292
Davi-code-debug <oluwaseyioke20@gmail.com>
2022-07-25 04:52:56 -0700

    task5

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds integers
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * returns 1 if error
+ * Return: 0 after success
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, k;
+	int sum;
+
+	sum = 0;
+
+	if (argc == 1)
+		printf("0");
+	for (i = 1; i <argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	for (k = 1; k < argc; k++)
+	{
+		sum = sum + atoi(argv[k]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
