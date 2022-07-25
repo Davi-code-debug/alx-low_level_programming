5a5bce8bd3093519b0dbecf1fe6bc2e9a45bba36
Davi-code-debug <oluwaseyioke20@gmail.com>
2022-07-25 03:26:30 -0700

    task2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints string
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * prints strings
+ * Return: 0 after success
+ */
+int main(int argc, char* argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
