ee87d75cc09012da27c3b86778e3851e3a8bbfe5
Davi-code-debug <oluwaseyioke20@gmail.com>
2022-07-27 12:38:07 -0700

    task1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - duplictaes string
+ * @str: address to string
+ *
+ * Return: pointer to string duplicate
+ */
+char *_strdup(char *str)
+{
+	char *ch;
+	unsigned int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	ch = malloc(sizeof(str));
+
+	if (ch == NULL)
+		return (ch);
+
+	for ( i = 0; str[i] != '\0'; i++)
+	{
+		*(ch + i) = *(str + i);
+	}
+	return (ch);
+}
