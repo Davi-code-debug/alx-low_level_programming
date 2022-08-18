9f82b6ed8f610587317a40984a94430ffdfa7e95
Davi-code-debug <oluwaseyioke20@gmail.com>
2022-08-18 16:03:30 -0700

    task4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,5 +1,10 @@
 #include "main.h"
 /**
+ * clear_bit - sets the value of a bit to 0 at a given index
+ * @n: pointer to the integer
+ * @index: index of the value to be set to 0
+ *
+ * Return: 1 if successful, -1 if an error occurred
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
