3e09fa5b4917dba8b42605d3c9752c478f1c4e07
Davi-code-debug <oluwaseyioke20@gmail.com>
2022-08-22 18:34:09 -0700

    task0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,6 +16,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
 	byte = read(fd, buf, letters);
 
 	close(fd);
